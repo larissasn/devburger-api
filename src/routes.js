b28a334069b8067fdc777987c7a9c63f8746f792
Larissa Santos <106403123+larissasn@users.noreply.github.com>
2024-08-18 18:28:04 -0300

    feat: Criação do controller de user, que é responsável de receber as informações, fazer as validações, regras de negocio...

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,8 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    id: v4(),
-    name: 'Lima',
-    email: 'lima@gmail.com',
-    password_hash: '1111',
-  });
-  return res.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
