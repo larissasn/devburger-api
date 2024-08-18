bb96a270133657d3e7591a91db083dd37eaff664
Larissa Santos <106403123+larissasn@users.noreply.github.com>
2024-08-18 17:02:17 -0300

    feat: Conectado a model de user ao banco.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
-const { Router } = require('express');
+import { Router } from 'express';
+import { v4 } from 'uuid';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.sttus(200).json({ message: 'Hello World!' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Lima',
+    email: 'lima@gmail.com',
+    password_hash: '1111',
+  });
+  return res.status(201).json(user);
+});
 
-module.exports = routes;
+export default routes;
