0fa3195673070c49854a64d578256d37601a8654
Larissa Santos <106403123+larissasn@users.noreply.github.com>
2024-09-01 14:31:20 -0300

    fea:  Upload de imagens utilizando a biblioteca Multer. Criado a configuração pra onde os uploads vão e qual sera o nome deles.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import { resolve } from 'node:path';
 import routes from './routes';
 import './database';
+
 class App {
   constructor() {
     this.app = express();
@@ -10,6 +12,10 @@ class App {
 
   middlewares() {
     this.app.use(express.json());
+    this.app.use(
+      '/product-file',
+      express.static(resolve(__dirname, '..', 'uploads')),
+    );
   }
 
   routes() {
