bc05e4efd23384a088529b6763637a01cd12a85c
Larissa Santos <106403123+larissasn@users.noreply.github.com>
2024-09-01 16:22:40 -0300

    feat: Config do Token JWT pt.2

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProductController from './app/controllers/ProductController';
+import authMiddleware from './middlewares/auth';
 
 const routes = new Router();
 
@@ -11,6 +12,7 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+routes.use(authMiddleware);
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
 
