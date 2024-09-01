e75019bdb3cfd88331850985b09d5bb3c5c240ef
Larissa Santos <106403123+larissasn@users.noreply.github.com>
2024-09-01 15:14:39 -0300

    feat: Conceitos de JWT(JSON Web Token) - É uma forma ou padrão de token onde agente passa nas nossas requisições pra deixá-las mais seguras.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 import * as Yup from 'yup';
+import jwt from 'jsonwebtoken';
 import User from '../models/User';
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(request, response) {
@@ -40,6 +42,9 @@ class SessionController {
       name: user.name,
       email: user.email,
       admin: user.admin,
+      token: jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
     });
   }
 }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd201d79454715d43f2049a2bd9f3eb32',
+  expiresIn: '5d',
+};
