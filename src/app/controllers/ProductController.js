e66ef34df5cd4113a84f1cee572323a16ac9cbc2
Larissa Santos <106403123+larissasn@users.noreply.github.com>
2024-09-05 21:38:52 -0300

    feat: Update de produto

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -9,6 +9,7 @@ class ProductController {
       name: Yup.string().required(),
       price: Yup.number().required(),
       category_id: Yup.number().required(),
+      offer: Yup.boolean(),
     });
 
     try {
@@ -24,16 +25,93 @@ class ProductController {
     }
 
     const { filename: path } = request.file;
-    const { name, price, category_id } = request.body;
+    const { name, price, category_id, offer } = request.body;
 
     const product = await Product.create({
       name,
       price,
       category_id,
       path,
+      offer,
     });
 
     return response.status(201).json(product);
+  }
+
+  async update(request, response) {
+    const schema = Yup.object({
+      name: Yup.string(),
+      price: Yup.number().positive('Price must be positive'),
+      category_id: Yup.number(),
+      offer: Yup.boolean(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    // if (!request.userId) {
+    //   return response.status(401).json({ error: 'Unauthorized' });
+    // }
+
+    // const user = await User.findByPk(request.userId);
+    // if (!user || !user.admin) {
+    //   return response.status(401).json({ error: 'Unauthorized' });
+    // }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId);
+
+    if (!isAdmin) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const { id } = request.params;
+    const findProduct = await Product.findByPk(id);
+    if (!findProduct) {
+      return response
+        .status(400)
+        .json({ error: 'Make sure your product ID is correct' });
+    }
+
+    let path;
+    if (request.file) {
+      path = request.file.filename;
+    }
+
+    const { name, price, category_id, offer } = request.body;
+
+    await Product.update(
+      {
+        name,
+        price,
+        category_id,
+        path,
+        offer,
+      },
+      {
+        where: {
+          id,
+        },
+      },
+    );
+
+    return response.status(200).json();
+    // const path = request.file ? request.file.filename : findProduct.path;
+
+    // try {
+    //   await Product.update(
+    //     { name, price, category_id, path, offer },
+    //     { where: { id } },
+    //   );
+
+    //   return response
+    //     .status(200)
+    //     .json({ message: 'Product updated successfully' });
+    // } catch (error) {
+    //   return response.status(500).json({ error: 'Internal Server Error' });
+    // }
   }
 
   async index(request, response) {
--- src/database/migrations/20240902225653-add-category-id-column.js
+++ src/database/migrations/20240902225653-add-category-id-column.js
@@ -1,19 +1,36 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('products', 'category_id', {
-      type: Sequelize.INTEGER,
-      references: {
-        model: 'categories',
-        key: 'id',
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'SET NULL',
-      allowNull: true,
-    });
+    // Verifica se a coluna "category_id" já existe na tabela "products"
+    const tableInfo = await queryInterface.describeTable('products');
+    // Se a coluna "category_id" não existir, adiciona-a
+    if (!tableInfo.category_id) {
+      await queryInterface.addColumn('products', 'category_id', {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      });
+    }
   },
+  // await queryInterface.addColumn('products', 'category_id', {
+  //   type: Sequelize.INTEGER,
+  //   references: {
+  //     model: 'categories',
+  //     key: 'id',
+  //   },
+  //   onUpdate: 'CASCADE',
+  //   onDelete: 'SET NULL',
+  //   allowNull: true,
+  // });
+  // },
 
   async down(queryInterface) {
+    // Remove a coluna "category_id" durante o rollback
     await queryInterface.removeColumn('products', 'category_id');
   },
 };
